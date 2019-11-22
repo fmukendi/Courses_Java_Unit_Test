005a4b939e7e698676b5e96dd2515e2537b32f1b
Franck Mukendi <franck_k_mukendi@homedepot.com>
2019-11-22 13:36:34 -0500

    I think that i'm done

--- 0001_CODE/unit-testing/src/main/java/com/mukeapps/unittesting/unittesting/service/SomeBusinessImpl.java
+++ 0001_CODE/unit-testing/src/main/java/com/mukeapps/unittesting/unittesting/service/SomeBusinessImpl.java
@@ -1,5 +1,9 @@
 package com.mukeapps.unittesting.unittesting.service;
 
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.OptionalInt;
+
 public class SomeBusinessImpl {
 
     private SomeDataService someDataService;
@@ -8,22 +12,27 @@ public class SomeBusinessImpl {
         this.someDataService = someDataService;
     }
 
-    public int calculateSum(int[] data){
-        int sum = 0;
+    public int calculateSum(int[] data) {
+        // functional programming
 
-        for(int value: data){
-            sum += value;
-        }
-        return  sum;
+        return Arrays.stream(data).reduce(Integer::sum).orElse(0);
+
+        // 1+2+3+4+5 => 1+2+3++5
+//        OptionalInt number =  Arrays.stream(data).reduce(Integer::sum);
+//        return number.orElse(0);
+//        int sum = 0;
+//        for(int value: data){
+//            sum += value;
+//        }
+//        return  sum;
     }
 
-    public int calculateSumWithDataService(){
+    public int calculateSumWithDataService() {
         int sum = 0;
-
         int[] data = someDataService.retrieveAllData();
-        for(int value: data){
-          sum += value;
+        for (int value : data) {
+            sum += value;
         }
-        return  sum;
+        return sum;
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Courses_Java_Unit_Test
+https://github.com/in28minutes/spring-unit-testing-with-junit-and-mockito
